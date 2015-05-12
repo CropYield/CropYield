aa9f9829941ea1e8dfe7553ea219f232b1a271e3
cpiggott <cpiggott@ksu.edu>
2015-05-12 02:22:53 -0500

    Added static website

---
+++ website/index.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<!--
+	Interphase by TEMPLATED
+	templated.co @templatedco
+	Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>SorghumYield</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-layers.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-xlarge.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+	<body class="landing">
+
+		<!-- Header -->
+			<header id="header">
+				<h1><a href="index.html">SorghumYield</a></h1>
+				<nav id="nav">
+					<ul>
+						<li><a href="index.html">Home</a></li>
+						<li><a href="generic.html">Info</a></li>
+						<li><a href="elements.html">Help</a></li>
+					</ul>
+				</nav>
+			</header>
+
+		<!-- Banner -->
+			<section id="banner">
+				<h2>SorghumYield</h2>
+				<p>A phrase about the application will go here.</p>
+				<ul class="actions">
+					<li>
+						<a href="#" class="button big">Learn More</a>
+					</li>
+				</ul>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="wrapper style1 align-center">
+				<div class="container">
+					<header>
+						<h2>Let SorghumYield Help You.</h2>
+						<p>SorghumYIeld is a tool made to assist in the prediction of year end yields.</p>
+					</header>
+					<div class="row 200%">
+						<section class="4u 12u$(small)">
+							<i class="icon big rounded fa-clock-o"></i>
+							<p>Traditional yield estimations are expensive and time consuming. SorghumYield can cut hours if not days out of the process.</p>
+						</section>
+						<section class="4u 12u$(small)">
+							<i class="icon big rounded fa-camera"></i>
+							<p>SorghumYield uses the most up-to-date image processing tools to analyze sorghum heads and calculate your yield.</p>
+						</section>
+						<section class="4u$ 12u$(small)">
+							<i class="icon big rounded fa-calculator"></i>
+							<p>SorghumYield factors in a variety of inputs to calculate the most accurate results possible.  </p>
+						</section>
+					</div>
+				</div>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="wrapper style2 align-center">
+				<div class="container">
+					<header>
+						<h2>How SorghumYield Works</h2>
+						<p>Below you will find a step-by-step guide on how to use SorghumYield to estimate your yields.</p>
+					</header>
+					<div class="row">
+						<section class="feature 6u 12u$(small)">
+							<img class="image fit" src="images/pic01.jpg" alt="" />
+							<h3 class="title">Step One. Download SorghumYield.</h3>
+							<p>Download the SorghumYield application from the Google Play Store. Once installed, set-up an account and move onto the next step.</p>
+						</section>
+						<section class="feature 6u$ 12u$(small)">
+							<img class="image fit" src="images/pic02.jpg" alt="" />
+							<h3 class="title">Step Two. Input Your Field Information.</h3>
+							<p>SorghumYield will ask for a few inputs such as: field name, heads per 1/1000th of an acre, row sizes. Once this information has been input, it's time to take images. </p>
+						</section>
+						<section class="feature 6u 12u$(small)">
+							<img class="image fit" src="images/pic03.jpg" alt="" />
+							<h3 class="title">Step Three. Take Pictures.</h3>
+							<p>Using the SorghumYield applicaiton, you will take pictures of your sorghum using the phones camera and the <a href="GRID_HEAD_SORGHUM.pdf" download="newfilename">SorghumYield Grid found here.</a> Once you have taken as many pictures as needed, click next and SorghumYield will analyze the images.</p>
+						</section>
+						<section class="feature 6u$ 12u$(small)">
+							<img class="image fit" src="images/pic04.jpg" alt="" />
+							<h3 class="title">Step Four. Output</h3>
+							<p>Once the images have been processed, you will be presented with a graph of your potential yields, as well as a slider to adjsut the seed weight. We also have given you access to view weather and soil information from this page. Once you are done, click save for later viewing.</p>
+						</section>
+					</div>
+					<footer>
+						<ul class="actions">
+							<li>
+								<!-- <a href="#" class="button alt big">Learn More</a> -->
+							</li>
+						</ul>
+					</footer>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+				<div class="container">
+					<div class="row">
+						<!-- <section class="4u 6u(medium) 12u$(small)">
+							<h3>Lorem ipsum</h3>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Assumenda, cumque!</p>
+							<ul class="alt">
+								<li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+								<li><a href="#">Quod adipisci perferendis et itaque.</a></li>
+								<li><a href="#">Itaque eveniet ullam, veritatis reiciendis?</a></li>
+								<li><a href="#">Accusantium repellat accusamus a, soluta.</a></li>
+							</ul>
+						</section>
+						<section class="4u 6u$(medium) 12u$(small)">
+							<h3>Nostrum, repellat!</h3>
+							<p>Tenetur voluptate exercitationem eius tempora! Obcaecati suscipit, soluta earum blanditiis.</p>
+							<ul class="alt">
+								<li><a href="#">Lorem ipsum dolor sit amet.</a></li>
+								<li><a href="#">Id inventore, qui necessitatibus sunt.</a></li>
+								<li><a href="#">Deleniti eum odit nostrum eveniet.</a></li>
+								<li><a href="#">Illum consectetur quibusdam eos corporis.</a></li>
+							</ul>
+						</section> -->
+						<section class="4u$ 12u$(medium) 12u$(small)">
+							<h3>Contact Us</h3>
+							<ul class="icons">
+								<li><a href="https://twitter.com/KSUCROPS" class="icon rounded fa-twitter"><span class="label">Twitter</span></a></li>
+								<li><a href="https://www.facebook.com/ksucrops" class="icon rounded fa-facebook"><span class="label">Facebook</span></a></li>
+								<!--<li><a href="#" class="icon rounded fa-pinterest"><span class="label">Pinterest</span></a></li>
+								<li><a href="#" class="icon rounded fa-google-plus"><span class="label">Google+</span></a></li>
+								<li><a href="#" class="icon rounded fa-linkedin"><span class="label">LinkedIn</span></a></li> -->
+							</ul>
+							<ul class="tabular">
+								<li>
+									<h3>Address</h3>
+									2014 Throckmorton Hall<br>
+									Kansas State University<br>
+									Manhattan, KS 66506
+								</li>
+								<li>
+									<h3>Mail</h3>
+									<a href="#">someone@untitled.tld</a>
+								</li>
+								<li>
+									<h3>Phone</h3>
+									(000) 000-0000
+								</li>
+							</ul>
+						</section>
+					</div>
+					<ul class="copyright">
+						<li>&copy; KSUCROPS. All rights reserved.</li>
+						<li>Design: <a href="http://templated.co">TEMPLATED</a></li><!-- 
+						<li>Images: <a href="http://unsplash.com">Unsplash</a></li> -->
+					</ul>
+				</div>
+			</footer>
+
+	</body>
+</html>
